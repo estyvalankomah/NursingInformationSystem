201eb0ef00c80178c54523cbf33ad8799637571c
estyvalankomah <abenammra2002@gmail.com>
2019-11-03 00:48:53 +0000

    Second commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.util.EventListener;
+
+public class Controller {
+    @FXML
+    private  Button vitalsBtn;
+
+//    vitalsBtn.setOnAction(new EventListener);
+}
