2339d40c560940243539a242cf52704b178e901b
Calebo1 <calebokyere1@gmail.com>
2019-11-04 05:54:50 +0000

    Fixed the StackPane Thing and send ing

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,52 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 
 import java.util.EventListener;
 
 public class Controller {
     @FXML
-    private  Button vitalsBtn;
+    private Button prescriptionBtn;
 
-//    vitalsBtn.setOnAction(new EventListener);
+    @FXML
+    private Button drugsBtn;
+
+    @FXML
+    private Button scheduleBtn;
+
+    @FXML
+    private Button logoutBtn;
+
+    @FXML
+    private Button vitalsBtn;
+
+    @FXML
+    private Pane vitalsPane;
+
+    @FXML
+    private Pane prescriptionPane;
+
+    @FXML
+    private Pane drugsPane;
+
+    @FXML
+    private Pane schedulePane;
+
+    @FXML   
+    public void onButtonClicked(ActionEvent event){
+        if(event.getSource() == vitalsBtn) {
+            vitalsPane.toFront();
+        }else if(event.getSource() == prescriptionBtn){
+            prescriptionPane.toFront();
+        }else if(event.getSource() == drugsBtn){
+            drugsPane.toFront();
+        }else if(event.getSource() == scheduleBtn){
+            schedulePane.toFront();
+        }else if(event.getSource() == logoutBtn){
+            System.out.println("User logged out");
+        }
+    }
 }
